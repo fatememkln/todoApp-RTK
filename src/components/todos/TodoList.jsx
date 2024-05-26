0b976362383c98a7ada31889f06823aadd82d492
fatememkln <fateme.mkln@gmail.com>
2024-05-26 12:13:42 +0330

    todos

--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -1,8 +1,8 @@
-
+import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-
+    const todos = useSelector((state) => state.todos);
   return (
     <div>
       <h2>TodoList</h2>
