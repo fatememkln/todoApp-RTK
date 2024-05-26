e4df0a2a9c739f8cdeae7d51f996349322bd5483
fatememkln <fateme.mkln@gmail.com>
2024-05-26 12:07:23 +0330

    build components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddTodoForm from "./components/Todos/AddTodoForm";
+import TodoList from "./components/Todos/TodoList";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="container pt-3">
+      <h1 className="text-center">TodoApp with RTK</h1>
+      <AddTodoForm />
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+export default App;
